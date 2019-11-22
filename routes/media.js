bc2aeb0dad4c41bed7cbc9a0a706075b9ec85378
DavidWiafe <wiafe.d@hotmail.com>
2019-11-22 17:53:15 -0500

    User can now save a media post

---
+++ routes/media.js
@@ -0,0 +1,102 @@
+﻿'use strict';
+/* 
+ File Name: media.js
+ Author: David Wiafe
+ Description: Handle register GET & POST request. 
+              Media CRUD
+*/
+var express = require('express');
+var router = express.Router();
+
+// require - media schema
+var Media = require('../models/media');
+
+// require - account schema
+var Account = require('../models/account');
+
+router.get('/', function (req, res) {
+    res.render('index' );
+});
+
+
+/* GET home page. */
+router.get('/media', function (req, res) {
+
+    console.log("req = " + req.user.username);
+
+    var date = new Date();
+
+    var day = date.getDate();
+
+    var month = date.getMonth() + 1;
+
+    var year = date.getFullYear();
+
+    var todaysDate = year +"-"+ month +"-"+ day;
+
+    console.log();
+   
+    res.render('media', { username: req.user.username, todayDate: todaysDate} );
+});
+
+
+// POST: 
+
+router.post('/media/add', function (req, res) {
+
+    console.log("test the date: " + req.body.dateTest);
+
+    res.render('media');
+
+    Media.create({
+
+
+        username: req.body.username,
+
+        date: req.body.theDate,
+
+        message: req.body.messageText
+
+
+    }, function (err, MediaPost) {
+
+        if (err) {
+
+            console.log(err);
+        } else {
+
+            res.render('media', { post: MediaPost });
+        }
+
+
+    });
+
+    
+
+
+});
+
+
+// get the login in user name
+function getUserName() {
+
+    
+
+
+}
+
+/* Check if the user is logged in or not */
+function LoggedInUser(req) {
+
+    if (req.isAuthenticated()) {
+
+        return true;
+
+    } else {
+
+        return false;
+    }
+}
+
+
+module.exports = router;
